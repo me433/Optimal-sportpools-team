186b5f8a3e75b72effb64ecd6a7e4e21eaad083d
unknown <milo_wera@hotmail.com>
2023-08-28 21:32:16 +0200

    started on sportpools startlist import function

--- index.py
+++ index.py
@@ -1,2 +1,22 @@
-def addition(a,b):
-    return a+b
+import requests
+import pandas as pd
+
+
+
+# every cyclist has (bib, name, cost, score)
+# dict(bib) -> (name, cost, score)
+
+startlist = {}
+
+# import url into table
+def import_url(url):
+    html = requests.get(url).content
+    df_list = pd.read_html(html)
+    df = df_list[0]
+    print(df)
+
+
+
+# add cyclists to startlist
+def populate_startlist():
+    return
